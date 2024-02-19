2df3f807f7874339603d7c5cde4a32fd95369c43
steveBrayone <stevebrayoneeb@gmail.com>
2024-02-19 11:58:07 +0530

    blahhhhh

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MailPlainText.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
